459b5ec71593cf1a48e6a17786fb7f9f86fc3486
夕妄 <1040331024@qq.com>
2022-08-26 08:38:09 +0800

    2.8 html

--- src/线下/JavaSE/map/MapDemo1.java
+++ src/线下/JavaSE/map/MapDemo1.java
@@ -1,33 +1,33 @@
 package map;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MapDemo1 {
-    public static void main(String[] args) {
-        Map<String, Integer> map = new HashMap<>();
-        map.put("语文", 99);
-        map.put("数学", 98);
-        map.put("英语", 100);
-        map.put("物理", 97);
-        map.put("化学", 100);
-        System.out.println(map);
-
-        Integer value = map.put("语文", 98);
-        System.out.println(value);
-        System.out.println(map);
-
-        value = map.get("数学");
-        System.out.println(value);
-        value = map.get("英语");
-        System.out.println(value);
-
-        map.remove("英语");
-        System.out.println(map);
-
-        System.out.println(map.size());
-
-        System.out.println("是否存在key:"+map.containsKey("语文"));
-        System.out.println("是否存在value:"+map.containsValue(99));
+    public static void test(String msg, String csn) throws IOException {
+        try (
+                FileOutputStream fos = new FileOutputStream("./test/demo.txt");
+        ) {
+            try {
+                fos.write(msg.getBytes(csn));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (NullPointerException | FileNotFoundException e) {
+            System.out.println("出错了!");
+        }
     }
+
+        public static void main (String[]args){
+            try {
+                test(null,"1");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 }
+
+
+
+
