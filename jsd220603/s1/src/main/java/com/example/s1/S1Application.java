97c4ccb65091fb9a13700fd9977c0ca44594d95f
夕妄 <1040331024@qq.com>
2022-09-03 10:32:36 +0800

    4.0 Mysql

---
+++ jsd220603/s1/src/main/java/com/example/s1/S1Application.java
@@ -0,0 +1,13 @@
+package com.example.s1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S1Application.class, args);
+    }
+
+}
