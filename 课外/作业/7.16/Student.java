1dc01855ba774ea26a115b5e0b170e9da8d8c636
zXiWang <72776076+zXiWang@users.noreply.github.com>
2022-11-09 19:55:36 +0800

    Delete 课外 directory

--- 课外/作业/7.16/Student.java
+++
@@ -1,9 +0,0 @@
-public class Student extends Person {
-    Student(String name, int age, String gender, int salary) {
-        super(name, age, gender, salary);
-    }
-
-    void study() {
-        System.out.println("在学习");
-    }
-}
--- 课外/课件/apiDay02/二进制演示工具/bintools.bat
+++
@@ -1,1 +0,0 @@
-java -jar BinTools.jar
